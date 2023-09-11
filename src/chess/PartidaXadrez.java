e3a363fd5163ae0cc4925b4f3e67c7efba3ce57a
Leonardo Infante <leosargino@gmail.com>
2023-09-11 19:36:50 -0300

    jogada especial roque

--- src/chess/PartidaXadrez.java
+++ src/chess/PartidaXadrez.java
@@ -105,6 +105,23 @@ public class PartidaXadrez {
             pecasNoTabuleiro.remove(pecaCapturada);
             pecasCapturadas.add(pecaCapturada);
         }
+        // roque pequeno 
+        if(p instanceof Rei && destino.getColuna() == origem.getColuna() + 2){
+            Posicao origemT = new Posicao(origem.getLinha(), origem.getColuna() + 3);
+            Posicao destinoT = new Posicao(origem.getLinha(), origem.getColuna() + 1);
+            PecaXadrez torre = (PecaXadrez)tabuleiro.removePeca(origemT);
+            tabuleiro.lugarPeca(torre, destinoT);
+            torre.aumentaContadorMovimento();
+        }
+        // roque grande 
+        if(p instanceof Rei && destino.getColuna() == origem.getColuna() - 2){
+            Posicao origemT = new Posicao(origem.getLinha(), origem.getColuna() - 4);
+            Posicao destinoT = new Posicao(origem.getLinha(), origem.getColuna() - 1);
+            PecaXadrez torre = (PecaXadrez)tabuleiro.removePeca(origemT);
+            tabuleiro.lugarPeca(torre, destinoT);
+            torre.aumentaContadorMovimento();
+        }
+        
         return pecaCapturada;
     }
     
@@ -117,6 +134,22 @@ public class PartidaXadrez {
             tabuleiro.lugarPeca(pecaCapturada, destino);
             pecasCapturadas.remove(pecaCapturada);
             pecasNoTabuleiro.add(pecaCapturada);
+        }
+        // roque pequeno 
+        if(p instanceof Rei && destino.getColuna() == origem.getColuna() + 2){
+            Posicao origemT = new Posicao(origem.getLinha(), origem.getColuna() + 3);
+            Posicao destinoT = new Posicao(origem.getLinha(), origem.getColuna() + 1);
+            PecaXadrez torre = (PecaXadrez)tabuleiro.removePeca(destinoT);
+            tabuleiro.lugarPeca(torre, origemT);
+            torre.diminuiContadorMovimento();
+        }
+        // roque grande 
+        if(p instanceof Rei && destino.getColuna() == origem.getColuna() - 2){
+            Posicao origemT = new Posicao(origem.getLinha(), origem.getColuna() - 4);
+            Posicao destinoT = new Posicao(origem.getLinha(), origem.getColuna() - 1);
+            PecaXadrez torre = (PecaXadrez)tabuleiro.removePeca(destinoT);
+            tabuleiro.lugarPeca(torre, origemT);
+            torre.diminuiContadorMovimento();
         }
     }
     
@@ -209,7 +242,7 @@ public class PartidaXadrez {
         novoLugarPeca('b', 1, new Cavalo(tabuleiro, Cor.BRANCO));
         novoLugarPeca('c', 1, new Bispo(tabuleiro, Cor.BRANCO));
         novoLugarPeca('d', 1, new Rainha(tabuleiro, Cor.BRANCO));
-        novoLugarPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO));
+        novoLugarPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO, this));
         novoLugarPeca('f', 1, new Bispo(tabuleiro, Cor.BRANCO));
         novoLugarPeca('g', 1, new Cavalo(tabuleiro, Cor.BRANCO));
         novoLugarPeca('h', 1, new Torre(tabuleiro, Cor.BRANCO));
@@ -226,7 +259,7 @@ public class PartidaXadrez {
         novoLugarPeca('b', 8, new Cavalo(tabuleiro, Cor.PRETO));
         novoLugarPeca('c', 8, new Bispo(tabuleiro, Cor.PRETO));
         novoLugarPeca('d', 8, new Rainha(tabuleiro, Cor.PRETO));
-        novoLugarPeca('e', 8, new Rei(tabuleiro, Cor.PRETO));
+        novoLugarPeca('e', 8, new Rei(tabuleiro, Cor.PRETO, this));
         novoLugarPeca('f', 8, new Bispo(tabuleiro, Cor.PRETO));
         novoLugarPeca('g', 8, new Cavalo(tabuleiro, Cor.PRETO));
         novoLugarPeca('h', 8, new Torre(tabuleiro, Cor.PRETO));
